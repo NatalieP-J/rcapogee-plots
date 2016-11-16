40292fe317dd66a05d25ac960156450165f646c8
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-11-16 18:37:36 -0500

    Started analysis of nearby Gaia stars for some clusters

--- filter_function.py
+++ filter_function.py
@@ -2,6 +2,6 @@ import numpy as np
 
 def starFilter(data):
 	"""
-	red_clump_12_TEFF_up4900.0_lo4800.0
+	clusters_12_CLUSTER_matchN7789
 	"""
-	return (data['TEFF'] < 4900.0) & (data['TEFF'] > 4800.0)
+	return (data['CLUSTER'] == "N7789")
