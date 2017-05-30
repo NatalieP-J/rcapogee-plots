9b634046e3ba9c9a8fe82ac23c68e6c18d6c5bec
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2017-05-30 12:58:26 -0400

    Added more external scripts

---
+++ sample_jackknife.py
@@ -0,0 +1,11 @@
+from empca_residuals import *
+
+datadir = '/geir_data/scr/price-jones/Data/apogee_dim_reduction/'
+sample_type = 'red_clump'
+bmask = 7935
+maxsamp = 5
+
+sample = empca_residuals('apogee',sample_type,maskFilter,ask=True,degree=2,datadir=datadir,
+                         badcombpixmask = bmask)
+
+sample.samplesplit(subsamples=25,maxsamp=maxsamp,varfuncs=[meanMed])
